e7ebc2144ac5bbd93cebac82a2e3b0646b2a4306
raulp <raulp@DESKTOP-9IMU10S>
2024-10-04 10:54:01 +0200

    Implementacion Empleado

---
+++ src/pkg/Empleado.java
@@ -0,0 +1,43 @@
+package pkg;
+
+public class Empleado {
+	
+	public enum TipoEmpleado{Vendedor, Encargado};
+	float salarioBase = 0;
+	float primas = 0;
+	float extras = 0;
+	
+	public float calculaNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+		//Determinar salario base
+		if(tipo == TipoEmpleado.Vendedor) {
+			salarioBase = 2000;
+		}else if(tipo == TipoEmpleado.Encargado) {
+			salarioBase = 2500;
+		}
+		
+		//Determinar primas 
+		if(ventasMes>=1500) {
+			primas = 200;
+		}else if(ventasMes>=100) {
+			primas = 100;
+		}
+		
+		//Determinar horas extras
+		extras = horasExtra * 30;
+		
+		return salarioBase + primas + extras;
+			
+	}
+	
+	public float calculoNominaNeta(float nominaBruta) {
+		float retencion = 0;
+		if(nominaBruta>=2500) {
+			retencion = 0.18f;
+		}else if(nominaBruta>=2100) {
+			retencion = 0.15f;
+		}
+		
+		return nominaBruta*(1-retencion);
+	}
+
+}
